e694a5bea4ab3da0b466aafbc54f8672c406445d
Propo41 <aliahnaf327@gmail.com>
2025-01-11 09:56:33 +0600

    added chrome extension redirect prompt after authentication

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,23 +1,52 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { useEffect, useState } from 'react';
 import TopNavigationBar from './TopNavigationBar';
 import BookRoomView from './BookRoomView';
 import MyEventsView from './MyEventsView';
-import { useLocation } from 'react-router-dom';
-import toast from 'react-hot-toast';
+import { Link, useLocation } from 'react-router-dom';
+import CelebrationRoundedIcon from '@mui/icons-material/CelebrationRounded';
+
+const ExtensionRedirectPrompt = () => {
+  return (
+    <Box
+      sx={{
+        flexGrow: 1,
+        position: 'relative',
+      }}
+    >
+      <Box
+        display={'flex'}
+        alignItems={'center'}
+        sx={{
+          flexDirection: 'column',
+          zIndex: 100,
+          mt: 20,
+          px: 4,
+        }}
+      >
+        <CelebrationRoundedIcon fontSize="large" />
+        <Typography variant="h5" mt={2}>
+          You're all set!
+        </Typography>
+        <Typography fontSize="1em" mt={2}>
+          Jump back into the extension to get started, or you may explore the{' '}
+          <Link to={'/'} onClick={() => window.location.reload()}>
+            webapp
+          </Link>
+        </Typography>
+      </Box>
+    </Box>
+  );
+};
 
 export default function Home() {
   const [tabIndex, setTabIndex] = useState(0);
   const { state } = useLocation();
+  const [extensionRedirectMessage, setExtensionRedirectMessage] = useState(null);
 
   useEffect(() => {
     const message = state?.message;
-    if (message) {
-      toast.success(message, {
-        duration: 10000,
-        icon: '🎉',
-      });
-    }
+    setExtensionRedirectMessage(message);
   }, []);
 
   const onRoomBooked = () => {
@@ -27,6 +56,10 @@ export default function Home() {
   const handleTabChange = (newValue: number) => {
     setTabIndex(newValue);
   };
+
+  if (extensionRedirectMessage) {
+    return <ExtensionRedirectPrompt />;
+  }
 
   return (
     <Box
@@ -52,7 +85,6 @@ export default function Home() {
           handleTabChange={handleTabChange}
         />
       </Box>
-
       {tabIndex === 0 && <BookRoomView onRoomBooked={onRoomBooked} />}
       {tabIndex === 1 && <MyEventsView />}
     </Box>
