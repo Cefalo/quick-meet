6e3d282144a557c9eef416cbb96153e73b8f627d
Parvez Ahammed <63257806+piru72@users.noreply.github.com>
2025-01-17 17:47:22 +0600

    Feat/issue 163 - Attendee Profile photo and Name to be displayed in the suggestions (#169)
    * add: initial layout for the image and name #163
    
    * add: attendee information formation
    
    * add: attendee dropdown shown
    
    * feat: attendees can be choosen from dropdown #163
    
    * fix: types and checks
    
    * fix: color used from palette
    
    * fix: interface renamed
    
    * fix: dynamic width

--- server/src/calender/calender.service.ts
+++ server/src/calender/calender.service.ts
@@ -3,7 +3,7 @@ import { BadRequestException, ConflictException, ForbiddenException, Inject, Inj
 import { calendar_v3 } from 'googleapis';
 import { extractRoomByEmail, isRoomAvailable, validateEmail } from './util/calender.util';
 import { AuthService } from '../auth/auth.service';
-import { DeleteResponse, EventResponse, EventUpdateResponse, IConferenceRoom } from '@quickmeet/shared';
+import { DeleteResponse, EventResponse, EventUpdateResponse, IConferenceRoom, IPeopleInformation } from '@quickmeet/shared';
 import { GoogleApiService } from 'src/google-api/google-api.service';
 
 @Injectable()
@@ -417,18 +417,19 @@ export class CalenderService {
     return floors;
   }
 
-  async searchPeople(client: OAuth2Client, emailQuery: string): Promise<string[]> {
-    const people = await this.googleApiService.searchPeople(client, emailQuery);
-    const emails = [];
-    for (const p of people) {
-      for (const email of p.emailAddresses) {
-        if (email.metadata.primary && email.metadata.verified) {
-          emails.push(email.value);
-          break;
-        }
-      }
-    }
+  async searchPeople(client: OAuth2Client, emailQuery: string): Promise<IPeopleInformation[]> {
+    const response = await this.googleApiService.searchPeople(client, emailQuery);
+    const peoples: IPeopleInformation[] = response.map((people) => {
+      const email = people.emailAddresses.find((email) => email.metadata.primary && email.metadata.verified);
+      const photo = people.photos?.find((photo) => photo.metadata.primary);
+      const name = people.names?.find((name) => name.metadata.primary);
+      return {
+        email: email?.value,
+        name: name?.displayName,
+        photo: photo?.url,
+      };
+    });
 
-    return emails;
+    return peoples;
   }
 }
