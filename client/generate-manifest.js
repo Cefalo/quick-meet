dd91d6aa474dc00bab03ddb6372bc09369f0490b
Propo41 <aliahnaf327@gmail.com>
2025-02-07 12:21:22 +0600

    added build step to bundle chrome package as artifact

--- client/generate-manifest.js
+++ client/generate-manifest.js
@@ -6,10 +6,11 @@ config();
 const hostPermission = `${process.env.VITE_BACKEND_ENDPOINT}/*`;
 const appDescription = process.env.VITE_APP_SLOGAN;
 const appTitle = process.env.VITE_APP_TITLE;
+const buildNumber = process.env.VITE_BUILD_NUMBER || 0;
 
 const manifest = {
   name: appTitle,
-  version: '1.0.4',
+  version: `1.1.${buildNumber}`,
   description: appDescription,
   manifest_version: 3,
   key: 'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAstDgQFGfJrfxo3id/1KTHVZpKbsCRMKZJXDUsDT8JRfasB/CeDGmuVs1hFYBJcGgn9PbK/mnE9hzWERVFpa4sfGZ3o0lvyfPLUfJd7PmfZ/4PQvE4+GfVQPAz/p4OVtP1WtbN4DED3jmrXiSrZ72paioz/ydVOSRDfo8m3+s9K92LcraYXHItvs+rSKXgfKxflfGzByme/fVO2V4yvE6T0YOqdLDc2USF4KGx0llHvqnmtB2K+rLr3V1/UcM1b4fP6kCiZAo7K2Tngpqa8DxgLVp8GYZ7NPPFJqu4tG1G1iRjtwk8Qblqmw+jmH+qZ2WguGtFpxU7P2JD8znPu//OwIDAQAB',
